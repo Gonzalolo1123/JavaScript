a121b812e292bd315b0012d05212d63543c78100
Gonzalolo1123 <gonzalolorenzo69@gmail.com>
2023-09-06 16:55:43 -0300

    Commit 1

---
+++ Test.js
@@ -0,0 +1,9 @@
+// Este es un comentario en JavaScript
+
+// Definimos una función que muestra un mensaje de alerta
+function mostrarMensaje() {
+  alert("¡Hola, mundo!");
+}
+
+// Llamamos a la función cuando la página se carga por completo
+window.onload = mostrarMensaje;
